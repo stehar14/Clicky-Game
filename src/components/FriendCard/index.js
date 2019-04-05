2745dbe2bcfbd3e93b51cbdce848881bb2347f45
Steve H <stehar14@yahoo.com>
2019-04-04 21:10:49 -0400

    Fix

---
+++ src/components/FriendCard/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function FriendCard(props) {
+  return (
+    
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+        
+      </span>
+    </div>
+    
+  );
+}
+
+export default FriendCard;
